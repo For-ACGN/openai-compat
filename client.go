1d8c186dad2ff4b989102ec309063d6dc081db2e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-24 21:39:21 +0800

    add project files about core client, and import github.com/stretchr/testify.

---
+++ client.go
@@ -0,0 +1,8 @@
+package openai
+
+type Client struct {
+}
+
+func NewClient() *Client {
+	return &Client{}
+}
---
+++ client_test.go
@@ -0,0 +1,11 @@
+package openai
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestClient(t *testing.T) {
+	require.NoError(t, nil)
+}
