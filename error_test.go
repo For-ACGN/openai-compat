bd2a1d442b13c29cb195fc7480566f44eebfa424
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-24 23:16:21 +0800

    add TestHandleError for check the simple invalid path error.

--- error_test.go
+++ error_test.go
@@ -1,1 +1,26 @@
 package openai
+
+import (
+	"fmt"
+	"testing"
+	
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandleError(t *testing.T) {
+	client := testNewClient(t)
+	
+	req, err := client.newGETRequest("models_invalid")
+	require.NoError(t, err)
+	
+	resp, err := client.do(req)
+	require.NoError(t, err)
+	
+	apiErr := handleError(resp)
+	ae, ok := apiErr.(*APIError)
+	require.True(t, ok)
+	fmt.Println(ae)
+	
+	err = client.Close()
+	require.NoError(t, err)
+}
