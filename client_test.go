72e4b957fd6368cbdca72fc4fc02ca0d492b9188
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-24 22:26:29 +0800

    add testBaseURL, testNewClient and TestNewClient.

--- client_test.go
+++ client_test.go
@@ -1,11 +1,30 @@
 package openai
 
 import (
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestClient(t *testing.T) {
-	require.NoError(t, nil)
+const (
+	testBaseURL = "https://api.xiaomimimo.com/v1"
+)
+
+func testNewClient(t *testing.T) *Client {
+	apiKey := os.Getenv("API_KEY")
+	opts := Options{
+		Timeout: 30 * time.Second,
+	}
+	client, err := NewClient(testBaseURL, apiKey, &opts)
+	require.NoError(t, err)
+	return client
+}
+
+func TestNewClient(t *testing.T) {
+	client := testNewClient(t)
+
+	err := client.Close()
+	require.NoError(t, err)
 }
