e0c27d83ff2b274e78a3f1c9ea165667a1327f32
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-25 20:21:02 +0800

    update TestHandleError about client.do, add context.

--- error_test.go
+++ error_test.go
@@ -1,26 +1,27 @@
 package openai
 
 import (
+	"context"
 	"fmt"
 	"testing"
-	
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestHandleError(t *testing.T) {
 	client := testNewClient(t)
-	
+
 	req, err := client.newGETRequest("models_invalid")
 	require.NoError(t, err)
-	
-	resp, err := client.do(req)
+
+	resp, err := client.do(context.Background(), req)
 	require.NoError(t, err)
-	
+
 	apiErr := handleError(resp)
 	ae, ok := apiErr.(*APIError)
 	require.True(t, ok)
 	fmt.Println(ae)
-	
+
 	err = client.Close()
 	require.NoError(t, err)
 }
