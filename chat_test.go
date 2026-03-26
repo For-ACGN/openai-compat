307b24841e10de2176a20583f650368fb85e739f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-26 21:34:53 +0800

    add sub test about thinking, merge common sub test.

--- chat_test.go
+++ chat_test.go
@@ -11,36 +11,81 @@ import (
 func TestClient_CreateChatCompletion(t *testing.T) {
 	client := testNewClient(t)
 
-	req := NewChatCompletionRequest(false)
-	req.Model = MiMoV2Omni
-	req.Messages = []*ChatCompletionMessage{
-		{
-			Role:    RoleSystem,
-			Content: "I'm writing a test, so please add prefix <test> in response",
-		},
-		{
-			Role:    RoleUser,
-			Content: "Hello LLM!",
-		},
-	}
+	t.Run("common", func(t *testing.T) {
+		req := NewChatCompletionRequest(false)
+		req.Model = MiMoV2Omni
+		req.Messages = []*ChatCompletionMessage{
+			{
+				Role:    RoleSystem,
+				Content: "I'm writing a test, so please add prefix <test> in response",
+			},
+			{
+				Role:    RoleUser,
+				Content: "Hello LLM!",
+			},
+		}
 
-	resp, err := client.CreateChatCompletion(req)
+		resp, err := client.CreateChatCompletion(req)
+		require.NoError(t, err)
+
+		require.NotEmpty(t, resp.ID)
+		require.Equal(t, MiMoV2Omni, resp.Model)
+		require.NotEmpty(t, resp.Choices)
+		require.NotZero(t, resp.Usage)
+		require.NotZero(t, resp.Created)
+
+		response := resp.Choices[0].Message.Content
+		fmt.Println(response)
+		require.Contains(t, response, "<test>")
+
+		fmt.Println(resp.ID)
+		fmt.Println(resp.Model)
+		fmt.Println(resp.Created)
+
+		spew.Dump(resp.Choices)
+		spew.Dump(resp.Usage)
+	})
+
+	t.Run("thinking", func(t *testing.T) {
+		req := NewChatCompletionRequest(false)
+		req.Model = MiMoV2Omni
+		req.Messages = []*ChatCompletionMessage{
+			{
+				Role:    RoleSystem,
+				Content: "I'm writing a test, so please add prefix <test> in response",
+			},
+			{
+				Role:    RoleUser,
+				Content: "Hello LLM!",
+			},
+		}
+		req.Thinking = true
+
+		resp, err := client.CreateChatCompletion(req)
+		require.NoError(t, err)
+
+		require.NotEmpty(t, resp.ID)
+		require.Equal(t, MiMoV2Omni, resp.Model)
+		require.NotEmpty(t, resp.Choices)
+		require.NotZero(t, resp.Usage)
+		require.NotZero(t, resp.Created)
+
+		response := resp.Choices[0].Message.Content
+		fmt.Println(response)
+		require.Contains(t, response, "<test>")
+
+		reason := resp.Choices[0].Message.ReasoningContent
+		fmt.Println(reason)
+		require.NotEmpty(t, reason)
+
+		fmt.Println(resp.ID)
+		fmt.Println(resp.Model)
+		fmt.Println(resp.Created)
+
+		spew.Dump(resp.Choices)
+		spew.Dump(resp.Usage)
+	})
+
+	err := client.Close()
 	require.NoError(t, err)
-
-	require.NotEmpty(t, resp.ID)
-	require.Equal(t, MiMoV2Omni, resp.Model)
-	require.NotEmpty(t, resp.Choices)
-	require.NotZero(t, resp.Usage)
-	require.NotZero(t, resp.Created)
-
-	response := resp.Choices[0].Message.Content
-	fmt.Println(response)
-	require.Contains(t, response, "<test>")
-
-	fmt.Println(resp.ID)
-	fmt.Println(resp.Model)
-	fmt.Println(resp.Created)
-
-	spew.Dump(resp.Choices)
-	spew.Dump(resp.Usage)
 }
