a32a6f98f4527afbaa9af6c74122677d6b8773a7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-27 22:00:27 +0800

    add sub tests about multi text content and with image content.

--- chat_test.go
+++ chat_test.go
@@ -8,6 +8,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	testImageURL = "https://example-files.cnbj1.mi-fds.com/example-files/image/image_example.png"
+	testAudioURL = "https://example-files.cnbj1.mi-fds.com/example-files/audio/audio_example.wav"
+	testVideoURL = "https://example-files.cnbj1.mi-fds.com/example-files/video/video_example.mp4"
+)
+
 func TestClient_CreateChatCompletion(t *testing.T) {
 	client := testNewClient(t)
 
@@ -121,6 +127,94 @@ func TestClient_CreateChatCompletion(t *testing.T) {
 
 		spew.Dump(resp.Choices)
 		spew.Dump(resp.Usage)
+	})
+
+	t.Run("multi text content", func(t *testing.T) {
+		req := NewChatCompletionRequest(false)
+		req.Model = MiMoV2Omni
+		req.Messages = []*ChatCompletionMessage{
+			{
+				Role:    RoleSystem,
+				Content: "I'm writing a test, so please add prefix <test> in response",
+			},
+			{
+				Role: RoleUser,
+				Content: []*Content{
+					{Text: "Hello"},
+					{Text: "LLM!"},
+				},
+			},
+		}
+
+		resp, err := client.CreateChatCompletion(req)
+		require.NoError(t, err)
+
+		require.NotEmpty(t, resp.ID)
+		require.Equal(t, MiMoV2Omni, resp.Model)
+		require.NotEmpty(t, resp.Choices)
+		require.NotZero(t, resp.Usage)
+		require.NotZero(t, resp.Created)
+
+		response := resp.Choices[0].Message.Content
+		fmt.Println(response)
+		require.Contains(t, response, "<test>")
+
+		fmt.Println(resp.ID)
+		fmt.Println(resp.Model)
+		fmt.Println(resp.Created)
+
+		spew.Dump(resp.Choices)
+		spew.Dump(resp.Usage)
+	})
+
+	t.Run("with image content", func(t *testing.T) {
+		t.Run("url", func(t *testing.T) {
+			req := NewChatCompletionRequest(false)
+			req.Model = MiMoV2Omni
+			req.Messages = []*ChatCompletionMessage{
+				{
+					Role:    RoleSystem,
+					Content: "I'm writing a test, so please add prefix <test> in response",
+				},
+				{
+					Role: RoleUser,
+					Content: []*Content{
+						{Text: "What is in this picture?"},
+						{ImageURL: testImageURL},
+					},
+				},
+			}
+
+			resp, err := client.CreateChatCompletion(req)
+			require.NoError(t, err)
+
+			require.NotEmpty(t, resp.ID)
+			require.Equal(t, MiMoV2Omni, resp.Model)
+			require.NotEmpty(t, resp.Choices)
+			require.NotZero(t, resp.Usage)
+			require.NotZero(t, resp.Created)
+
+			response := resp.Choices[0].Message.Content
+			fmt.Println(response)
+			require.Contains(t, response, "<test>")
+			require.Contains(t, response, "tree")
+			require.Contains(t, response, "light")
+
+			fmt.Println(resp.ID)
+			fmt.Println(resp.Model)
+			fmt.Println(resp.Created)
+
+			spew.Dump(resp.Choices)
+			spew.Dump(resp.Usage)
+		})
+	})
+
+	t.Run("with audio content", func(t *testing.T) {
+
+	})
+
+	t.Run("with video content", func(t *testing.T) {
+
 	})
 
 	err := client.Close()
