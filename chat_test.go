011343c331a527d626d325375eea982a29fcb53e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-27 21:08:02 +0800

    add sub test about set max tokens to TestClient_CreateChatCompletion.

--- chat_test.go
+++ chat_test.go
@@ -86,6 +86,43 @@ func TestClient_CreateChatCompletion(t *testing.T) {
 		spew.Dump(resp.Usage)
 	})
 
+	t.Run("max tokens", func(t *testing.T) {
+		req := NewChatCompletionRequest(false)
+		req.Model = MiMoV2Omni
+		req.Messages = []*ChatCompletionMessage{
+			{
+				Role:    RoleSystem,
+				Content: "I'm writing a test, so please add prefix <test> in response",
+			},
+			{
+				Role:    RoleUser,
+				Content: "Hello LLM!",
+			},
+		}
+		req.MaxTokens = 5
+
+		resp, err := client.CreateChatCompletion(req)
+		require.NoError(t, err)
+
+		require.NotEmpty(t, resp.ID)
+		require.Equal(t, MiMoV2Omni, resp.Model)
+		require.NotEmpty(t, resp.Choices)
+		require.NotZero(t, resp.Usage)
+		require.NotZero(t, resp.Created)
+
+		response := resp.Choices[0].Message.Content
+		fmt.Println(response)
+		require.Contains(t, response, "<test>")
+		require.Less(t, len(response), 20)
+
+		fmt.Println(resp.ID)
+		fmt.Println(resp.Model)
+		fmt.Println(resp.Created)
+
+		spew.Dump(resp.Choices)
+		spew.Dump(resp.Usage)
+	})
+
 	err := client.Close()
 	require.NoError(t, err)
 }
